c1faf053cc16b726c0ffb83841914bdbcc390c49
sharvarihupare-369 <sharvarihupare15@gmail.com>
2023-07-30 21:13:48 +0530

    add

---
+++ objectsbulk/index.js
@@ -0,0 +1,32 @@
+
+//Factory functions
+function createAccount(name,type,accBalance){
+    let account = {}
+    account.name = name
+    account.type = type
+    account.accBalance = accBalance
+    account.printdetails = function(){
+        console.log('Name',this.name,'Type',this.type)
+    }
+    return account 
+}
+
+let acc1 = createAccount("Sharvari","savings",1000)
+// console.log(acc1)
+// acc1.printdetails()
+
+
+function userDetails(firstname,lastname){
+    let user = {}
+    user.firstname = firstname
+    user.lastname = lastname
+    user.sayFullName = function(){
+        console.log(firstname,lastname)
+    }
+    return user
+}
+
+let u1 = userDetails("Sharvari","Hupare")
+console.log(u1)
+u1.sayFullName()
+ 
