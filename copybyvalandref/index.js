f534cb40dbf3c932b3d740eaf1dce69863d21aa9
sharvarihupare-369 <sharvarihupare15@gmail.com>
2023-07-30 18:20:51 +0530

    add

---
+++ copybyvalandref/index.js
@@ -0,0 +1,25 @@
+
+
+//primitive
+let user = "John"
+
+let newUser = user
+
+user = "Sam";
+console.log(user,"user")
+console.log(newUser,"newUser")
+
+//non primitive
+
+
+let data = {
+    name : "Sharvari",
+    age : 21
+}
+
+let userInfo = data
+
+userInfo.name = "Sharu"
+
+console.log(userInfo.name,'userInfo')
+console.log(data.name,'data')
