7bcd7a92c56943860adf3af341b808e70cf4d355
sharvarihupare-369 <sharvarihupare15@gmail.com>
2023-07-31 17:55:21 +0530

    add

--- assignments1/index.js
+++ assignments1/index.js
@@ -7,6 +7,9 @@ let to = "Arya Stark";
 /*
 1. Find the index of the first 'is' in the variable quote. And store it in a new variable named indexOfIs
 */
+let indexOfIs = quote.indexOf("is")
+console.log(indexOfIs)
+
 
 
 /*
@@ -14,9 +17,13 @@ let to = "Arya Stark";
 
 */
 
+
+
+
 /*
 3. Log the message saying `The index of first is in quote is 7`
 */
+console.log(`The index of first is in quote is ${quote.indexOf("is")+ 1}`)
 
 /*
 4. Log the message for first 6 characters of quote like this.
@@ -27,6 +34,10 @@ let to = "Arya Stark";
   The character at index 4 is 'e'
   The character at index 5 is ' '
 */
+// let qarr = quote.split(" ").forEach((el)=>{
+
+// })
+// console.log(qarr)
 
 
 /*
@@ -34,46 +45,70 @@ let to = "Arya Stark";
   "Syrio Forel said There is only one thing we say to death: Not today to Arya Stark." (use concat method)
 */
 
+console.log(from.concat(" ","said"," ",quote.concat(" ","to", " ",to)))
+console.log(`${from} said ${quote} to ${to}`)
+
 /*
 6. Does from, to and quote ends with "rk". Check all three.
 */
+
+console.log(from.endsWith("rk"))
+console.log(quote.endsWith("rk"))
+console.log(to.endsWith("rk"))
 
 /*
 7. Does quote includes the word "Only"
 
 */
 
+console.log(quote.includes("Only"))
+
+
 /*
 8. Does quote includes the word " we"
 */
+
+console.log(quote.includes("we"))
+
 
 /*
 9. Find the index of the the word `we` in quote
 */
 
+console.log(quote.indexOf("we"))
+
 /*
 10. Split the quote into individual word and store it in a variable name quoteSplitted
 */
+
+let quoteSplitted = quote.split(" ")
+console.log(quoteSplitted)
 
 
 /*
 11. Change the word "today" in quoteSplitted to "tomorrow" and join all the words to form a sentance.
 */
 
+let newquote = quoteSplitted.join(" ").replace("today","tomorrow")
+console.log(newquote)
+
 
 /*
 12. Find the index of second "o" in quote. Use indexOf
 */
 
+// console.log(quote.indexOf("o",2))
 
 /*
 13. Find the last index of letter "a" in quote.
 */
 
 
+
 /*
 14. Find the second last index of letter "a" in quote.
 */
+
 
 /*
 15. Make the quote 70 character long. If it has less characters add rest as .......
